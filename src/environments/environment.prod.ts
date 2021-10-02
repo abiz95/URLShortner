9ad73217fcd359db0d43b1a622ed97de3d7dce30
abiz95 <abintitusjohn@gmail.com>
2021-10-03 01:59:08 +0530

    properties updated

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,11 @@
+// export const environment = {
+//   production: true,
+//   hostUrl: 'http://localhost:4200',
+//   apiUrl: 'http://localhost:8080'
+// };
+
 export const environment = {
   production: true,
-  hostUrl: 'http://localhost:4200',
-  apiUrl: 'http://localhost:8080'
+  hostUrl: 'https://zap-url-shortner.herokuapp.com',
+  apiUrl: 'https://zap-url.herokuapp.com'
 };
